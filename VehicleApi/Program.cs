f9427f5f41dc8c8a2162902527efe9776575eeb4
Zisis Zaverdas <zisiszaverdas@gmail.com>
2025-07-12 13:26:53 +0300

    Add models and data store for vehicle API
    Introduces `Category`, `Event`, and `Vehicle` classes.

--- VehicleApi/Program.cs
+++ VehicleApi/Program.cs
@@ -1,6 +1,9 @@
+using VehicleApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<DataStore>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
